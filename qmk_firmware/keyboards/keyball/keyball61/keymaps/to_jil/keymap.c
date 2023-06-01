b5f0906624ae13a187afd65305f2e3f30e85ae05
石川 大夢 <oumu@ishikawanoMacBook-Air-2.local>
2023-06-01 23:35:02 +0900

    Fix colors

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/to_jil/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/to_jil/keymap.c
@@ -58,16 +58,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 const rgblight_segment_t PROGMEM my_layer0_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {29, 15, HSV_CYAN}
+    {29, 8, HSV_CYAN}
+    {64, 7, HSV_CYAN}
 );
 const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {29, 15, HSV_MAGENTA}
+    {29, 8, HSV_MAGENTA}
+    {64, 7, HSV_MAGENTA}
 );
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {29, 15, HSV_CHARTREUSE}
+    {29, 8, HSV_CHARTREUSE}
+    {64, 7, HSV_CHARTREUSE}
 );
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {29, 15, HSV_RED}
+    {29, 8, HSV_RED}
+    {64, 7, HSV_RED}
 );
 
 // Now define the array of layers. Later layers take precedence
@@ -85,9 +89,9 @@ void keyboard_post_init_user(void) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     // Auto enable scroll mode when the highest layer is 3
-    rgblight_set_layer_state(1, layer_state_cmp(state, 1));
-    rgblight_set_layer_state(2, layer_state_cmp(state, 2));
-    rgblight_set_layer_state(3, layer_state_cmp(state, 3));
+    rgblight_set_layer_state(0, layer_state_cmp(state, 1));
+    rgblight_set_layer_state(1, layer_state_cmp(state, 2));
+    rgblight_set_layer_state(2, layer_state_cmp(state, 3));
     return state;
 }
 
